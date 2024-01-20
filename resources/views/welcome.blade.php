ba0560e385fee1fb48509a7f18d7d602ca4a64bf
raka1401 <dira221107@gmail.com>
2024-01-20 11:30:03 +0800

    anu template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 @extends('layout.master')
 
+@section('judul_halaman')
+    <h1>ini judul</h1>
+@endsection
+
 @section('content')
-<h1>isi content</h1>
-<h1>isi content</h1>
-<h1>isi content</h1>
-<h1>isi content</h1>
-<h1>isi content</h1>
-<h1>isi content</h1>
+<h1>isi content</h1><br>
+<h1>isi content</h1><br>
+<h1>isi content</h1><br>
+<h1>isi content</h1><br>
+<h1>isi content</h1><br>
+<h1>isi content</h1><br>
 @endsection
