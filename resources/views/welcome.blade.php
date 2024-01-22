fc89cee4c0d2051a2a1d5ea4f1e07a51b89e5225
raka1401 <dira221107@gmail.com>
2024-01-22 16:08:30 +0800

    ini betul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,5 @@
 @extends('layout.master')
 
-<<<<<<< HEAD
-tes berubahdsddfsdfdfdfdfd
-=======
 @section('judul_halaman')
     <h1>ini judul</h1>
 @endsection
@@ -15,4 +12,3 @@ tes berubahdsddfsdfdfdfdfd
 <h1>isi content</h1><br>
 <h1>isi content</h1><br>
 @endsection
->>>>>>> ba0560e385fee1fb48509a7f18d7d602ca4a64bf
